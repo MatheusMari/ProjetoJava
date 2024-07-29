af2a610aa50fd2b105e1ca748df9a760ec247b70
gabrielflorianoo <tacacheee221@gmail.com>
2024-07-29 18:46:32 -0300

    Paginas agora possuem imagens, adicionado campo imagen no criador de paginas

--- projeto/routes/paginas.js
+++ projeto/routes/paginas.js
@@ -1,31 +1,50 @@
-// Rota onde ficará armazenado todas as páginas criadas
-
 var express = require("express");
 var router = express.Router();
+var multer = require("multer");
+var path = require("path");
+var fs = require("fs"); // Para ler arquivos e diretórios
+
+// Configurar armazenamento do multer
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        var uploadDir = 'uploads/';
+        // Verificar se o diretório existe, caso contrário, criá-lo
+        if (!fs.existsSync(uploadDir)){
+            fs.mkdirSync(uploadDir);
+        }
+        cb(null, uploadDir);
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname)); // Renomear o arquivo com a data atual para evitar conflitos
+    }
+});
+
+var upload = multer({ storage: storage });
 
 /* GET paginas. */
 router.get("/", function (req, res, next) {
-	// Verificar se usuário está logado
-	if (!req.session.user) {
-        res.send('Usuário não conectado');
-	}
-
+    // Verificar se usuário está logado
+    if (!req.session.user) {
+        return res.send('Usuário não conectado');
+    }
     res.render('criadorPaginas');
 });
 
 /* POST paginas. */
-router.post("/", function (req, res, next) {
-	// Verificar se usuário está logado
-	if (!req.session.user) {
-        res.send('Usuário não conectado');
-	}
+router.post("/", upload.single('imagem'), function (req, res, next) {
+    // Verificar se usuário está logado
+    if (!req.session.user) {
+        return res.send('Usuário não conectado');
+    }
 
-	var pagina = {
+    var pagina = {
         titulo: req.body.titulo,
         url: req.body.url,
-        html: req.body.html
+        html: req.body.html,
+        imagem: req.file ? req.file.filename : null // Nome do arquivo da imagem salva
     };
 
+    req.paginas = req.paginas || [];
     req.paginas.push(pagina);
 
     res.redirect('/paginas/' + req.body.url);
@@ -33,10 +52,10 @@ router.post("/", function (req, res, next) {
 
 /* GET pagina criada */
 router.get("/:url", function (req, res, next) {
-	// Variável Boolean que retorna se encontrou dada url na lista de paginas
+    // Variável Boolean que retorna se encontrou dada url na lista de paginas
     var pagina = req.paginas.find(p => p.url === req.params.url);
 
-	// Renderiza o html na página
+    // Renderiza o html na página
     if (pagina) {
         res.send(pagina.html);
     } else {
